61fd5657f85adf05a8b04e598186ac000441c2a3
Nazeer-Akram <nazeerakram1@gmail.com>
2025-03-17 16:44:43 +0530

    JavaScript : Data Type

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+
+// int           => for 2 to 53
+// BigInt        =>
+// String        =>""
+// Boolean       => True / Flase
+// null          => Null
+// undefined     => undefined
+// Symbol
+// Object
