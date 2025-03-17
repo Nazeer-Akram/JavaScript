3dd28f477f27c4ea2b0c3c7e8691c07ba6e8e67b
Nazeer-Akram <nazeerakram1@gmail.com>
2025-03-17 16:51:09 +0530

    JavaScript : Data Type Converions

---
+++ dataTypeConversion.js
@@ -0,0 +1,51 @@
+
+//To Number
+
+/*
+let varValue = undefined;
+let strToNumber = Number(varValue);
+
+console.log(typeof(strToNumber));
+console.log(strToNumber);
+*/
+
+
+// "33"        => Number
+// "33abc"     => NaN
+// null        => 0
+// undefined   => NaN
+// true        => 1;   False   => 0
+
+
+//To Boolean
+/*
+let varValue =0;
+let numberToBol = Boolean(varValue);
+
+console.log(typeof(numberToBol));
+console.log(numberToBol);
+*/
+
+
+// "33"        => true
+// ""          => flase
+// null        => false
+// undefined   => false
+// 1           => true
+
+
+
+
+//To String
+
+let varValue =true;
+let BolToString = String(varValue);
+
+console.log(typeof(BolToString));
+console.log(BolToString);
+
+
+// 33          => string
+// null        => string as null
+// undefined   => string as undefined
+// 1           => string
