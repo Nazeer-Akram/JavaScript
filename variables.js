8e2b3f1b88f340e80013452c9123cb73688dcf13
Nazeer-Akram <nazeerakram1@gmail.com>
2025-03-17 16:42:33 +0530

    JavaScript : Variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Variables in JavaScript
+
+
+
+const id    = 123;               //This is constant variable never can be redeclear.
+var nameId = "nazeer";           //This is function Scoped variable and can be redeclear Recomended not to use.
+let passwordId = "nazeerakram";  //This is block Scoped variable and cannot be redeclear Recomended to use.
+
+
+
+
