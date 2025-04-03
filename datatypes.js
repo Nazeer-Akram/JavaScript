ad8a3a26b60e2b9df416b071cafb59184354cfdb
Nazeer-Akram <nazeerakram1@gmail.com>
2025-04-03 10:48:06 +0530

    JavaScript : Datatypes with console check.

--- datatypes.js
+++ datatypes.js
@@ -9,3 +9,16 @@
 // undefined     => undefined
 // Symbol
 // Object
+
+
+let varName = "string";
+let varNameI = 123456789;
+let varNameII = true;
+let varNameIII = 121212121212123456576869696504659461346518465891436584;
+let varnameIV = null;
+let varnameV;
+let varNameVI = {};
+// Symbol;
+
+
+console.log(typeof null);
